9dd4df2aad4eda475e9930fc9acc07198a391b2d
Ayman Karim <akarim1@terpmail.umd.edu>
2017-09-07 00:28:36 +0000

    Remove print statement in client

--- src/crystal_twitter/client.cr
+++ src/crystal_twitter/client.cr
@@ -24,7 +24,6 @@ module Twitter
             
             users = [] of Twitter::User
             
-            puts response.body
             Array(Twitter::User).new((JSON::PullParser.new(response.body)))
         end
         
