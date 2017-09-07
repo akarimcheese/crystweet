ff11e0815cbbb695ec41e7655d5bfd9ea88ef3e7
Ayman Karim <akarim1@terpmail.umd.edu>
2017-09-07 00:28:14 +0000

    Remove print statement in client

--- src/crystal_twitter.cr
+++ src/crystal_twitter.cr
@@ -1,4 +1,22 @@
 require "./crystal_twitter/*"
 
 module Twitter
+    Twitter::Client.new(
+        ENV["TWITTER_CONSUMER_KEY"], 
+        ENV["TWITTER_CONSUMER_SECRET"], 
+        ENV["TWITTER_ACCESS_TOKEN"],
+        ENV["TWITTER_ACCESS_SECRET"]
+    ).userSearch("Adam Grove", {"count" => "1"})
+    
+    # i = 0
+    
+    # Twitter::Client.new(
+    #     ENV["TWITTER_CONSUMER_KEY"], 
+    #     ENV["TWITTER_CONSUMER_SECRET"], 
+    #     ENV["TWITTER_ACCESS_TOKEN"],
+    #     ENV["TWITTER_ACCESS_SECRET"]
+    # ).getFollowersFor("johncena", {} of String=>Array(String))
+    # .sleep_on_rate_limit
+    # .each do |id|
+    # end
 end
