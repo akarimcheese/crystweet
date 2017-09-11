11c3f057f66147e744e5dc10344e6a41734729b1
Ayman Karim <akarim1@terpmail.umd.edu>
2017-09-11 03:13:45 +0000

    temp change

--- src/crystal_twitter/client.cr
+++ src/crystal_twitter/client.cr
@@ -59,6 +59,7 @@ module Twitter
             response = request.exec
             
             #Handle non-200
+            puts response.body
             Twitter::Cursor(UInt64).new(request, JSON::PullParser.new(response.body))
         end
     end
